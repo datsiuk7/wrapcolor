f68071bad4f6d1614006827c1b38e374ae38595c
datsiuk7 <bogdandatcuk@gmail.com>
2025-10-19 13:24:14 +0300

    v0.1.1

--- wrapcolor/__init__.py
+++ wrapcolor/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """wrapcolor: Universal ANSI colorizer for Python."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __all__ = ["_Color", "color"]
 
 from .color import _Color, color
